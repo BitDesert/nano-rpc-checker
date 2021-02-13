a1b2dd94e7e322f336189a3142c35dcea3acb126
BitDesert <11021604+BitDesert@users.noreply.github.com>
2021-02-13 12:58:53 +0100

    backends

---
+++ api/index.ts
@@ -0,0 +1,34 @@
+import { NowRequest, NowResponse } from '@vercel/node'
+import axios from 'axios';
+
+const backends = {
+  "ninja": "https://mynano.ninja/api/node",
+  "nanos": "https://nault.nanos.cc/proxy",
+  "voxpopuli": "https://vox.nanos.cc/api",
+  "powernode": "https://proxy.powernode.cc/proxy",
+  "rainstorm": "https://rainstorm.city/api"
+}
+
+export default (req: NowRequest, res: NowResponse) => {
+  const { backend } = req.query
+  
+  if(req.query.backend){
+    return res.status(400).json({error: 'No backend specified'})
+  }
+
+  if(!backends[backend]){
+    return res.status(400).json({error: 'Backend not found'})
+  }
+  
+  axios.post(backends[backend], {
+    action: 'version'
+  })
+  .then(function (response) {
+    console.log(response.data);
+    res.json(response.data)
+  })
+  .catch(function (error) {
+    console.log(error);
+    res.status(500).json(error)
+  });
+}
